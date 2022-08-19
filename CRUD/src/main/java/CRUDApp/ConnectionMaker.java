fdae2a974ba3624a521cf6fca2fdaa5bdb8d8f29
Yagnesh <pancholiyagnesh2001@gmail.com>
2022-08-19 11:40:18 +0530

    intial commit

---
+++ CRUD/src/main/java/CRUDApp/ConnectionMaker.java
@@ -0,0 +1,13 @@
+package CRUDApp;
+
+public class ConnectionMaker
+{
+
+	public static void main(String[] args)
+	{
+		String jdbcURL = "";
+		String userName = "root";
+		String password = "P@ssw0rd@123";
+	}
+
+}
